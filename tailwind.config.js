68424a9a6c9b55b7e05be9d6055c4335b9402ebc
Ian Virtue <ianvirtue101@gmail.com>
2023-05-01 14:39:44 -0400

    updated tailwind config file

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,18 +1,40 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
+    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
     extend: {
       backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
+        "gradient-conic":
+          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+      },
+      backgroundColor: {
+        primary: "#4CAE4C",
+        "primary-light": "#6BE06B",
+        "primary-dark": "#328532",
+        secondary: "#FFB800",
+        "secondary-light": "#FFD13F",
+        "secondary-dark": "#C68C00",
+        accent: "#E53A40",
+        "accent-light": "#FF6B6B",
+        "accent-dark": "#B02630",
+      },
+      textColor: {
+        primary: "#4CAE4C",
+        "primary-light": "#6BE06B",
+        "primary-dark": "#328532",
+        secondary: "#FFB800",
+        "secondary-light": "#FFD13F",
+        "secondary-dark": "#C68C00",
+        accent: "#E53A40",
+        "accent-light": "#FF6B6B",
+        "accent-dark": "#B02630",
       },
     },
   },
   plugins: [],
-}
+};
